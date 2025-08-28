4b93fdbaa706cc3ab64890e0ca2df16792b38d35
Will <will00987654@gmail.com>
2025-08-28 11:13:18 -0300

    Adição do arquivo middleware.ts (vazio/comentado)

---
+++ middleware.ts
@@ -0,0 +1,7 @@
+// Lógica do middleware.ts: Este arquivo terá um código que intercepta as requisições. A lógica será:
+
+// Verificar se o usuário está tentando acessar uma rota protegida (qualquer URL que comece com /dashboard, /inventory-query, etc.).
+
+// Verificar se existe um "token" ou "cookie" de sessão que prove que o usuário está logado. (Ainda vamos criar a lógica de login, por enquanto podemos simular).
+
+// Se ele tentar acessar uma rota protegida e não estiver logado, redirecione-o para a página /login.
